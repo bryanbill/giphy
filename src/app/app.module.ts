e4c7ff1025629294c3caaad5fab3e4864402854b
bryanbill <emobrya@gmail.com>
2022-04-13 20:09:46 +0300

    chore:scaffolding - created app components and services
    Co-authored-by: Christine <nyakiochristine@users.noreply.github.com>
    Co-authored-by: Elvis <DynastyElvis@users.noreply.github.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GiphyComponent } from './giphy/giphy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GiphyComponent
   ],
   imports: [
     BrowserModule,
