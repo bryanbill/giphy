42d6ea6391c85f3eda4aa21122288aeeb465dcc1
bryanbill <emobrya@gmail.com>
2022-04-14 15:32:04 +0300

    fix:cors - allow only safe urls

--- src/app/gif.service.ts
+++ src/app/gif.service.ts
@@ -13,14 +13,14 @@ export class GifService {
   getTrendingGifs() {
     // Paginate results by setting the limit to 25
     return this.http.get(
-      `http://api.giphy.com/v1/gifs/trending?api_key=${this.apiKey}&limit=25`
+      `https://api.giphy.com/v1/gifs/trending?api_key=${this.apiKey}&limit=25`
     );
   }
 
   // Get a list of GIFs by searching for a keyword
   getGifsByKeyword(keyword: string) {
     return this.http.get(
-      `http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${this.apiKey}&limit=25`
+      `https://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${this.apiKey}&limit=25`
     );
   }
 }
